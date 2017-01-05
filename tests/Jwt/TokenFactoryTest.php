a99708e234cd66c7b3ffb512f73a5da4d81fffe6
Radoslaw Mejer <radmen@desmart.com>
2017-01-05 17:10:50 +0100

    Add test suite for TokenFactoryTest::createFromRequest()

--- tests/Jwt/TokenFactoryTest.php
+++ tests/Jwt/TokenFactoryTest.php
@@ -2,31 +2,32 @@
 
 namespace test\JWTAuth\Jwt;
 
-use DeSmart\JWTAuth\Auth\Guard;
-use Lcobucci\JWT\Builder;
-use Lcobucci\JWT\Parser;
-use Lcobucci\JWT\Signer\Hmac;
-use Lcobucci\JWT\Token;
+use DeSmart\JWTAuth\Exception\InvalidTokenException;
 use DeSmart\JWTAuth\Jwt\TokenFactory;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac;
+use Illuminate\Http\Request;
 
 class TokenFactoryTest extends \PHPUnit_Framework_TestCase
 {
+    private $secret = 'somuchsecrets';
+
+    private $expireTtl = '1 week';
+
     /** @test */
     public function it_creates_token_for_user()
     {
-        $expireTtl = '1 week';
-        $secret = 'somuchsecrets';
         $now = new \DateTimeImmutable();
         $user = (object)['id' => 1];
-        $expireAt = $now->add(\DateInterval::createFromDateString($expireTtl));
+        $expireAt = $now->add(\DateInterval::createFromDateString($this->expireTtl));
 
         $expectedToken = (new Builder)->setIssuedAt($now->getTimestamp())
             ->setExpiration($expireAt->getTimestamp())
             ->set('uid', 1)
-            ->sign(new Hmac\Sha256, $secret)
+            ->sign(new Hmac\Sha256, $this->secret)
             ->getToken();
 
-        $factory = new TokenFactory($expireTtl, $secret, $now);
+        $factory = $this->createFactory($now);
 
         $this->assertEquals($expectedToken, $factory->createForUser($user));
     }
@@ -34,21 +35,86 @@ class TokenFactoryTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_creates_token_with_extra_claims()
     {
-        $expireTtl = '1 week';
-        $secret = 'somuchsecrets';
         $now = new \DateTimeImmutable();
         $user = (object)['id' => 1];
-        $expireAt = $now->add(\DateInterval::createFromDateString($expireTtl));
+        $expireAt = $now->add(\DateInterval::createFromDateString($this->expireTtl));
 
         $expectedToken = (new Builder)->setIssuedAt($now->getTimestamp())
             ->setExpiration($expireAt->getTimestamp())
             ->set('foo', 'bar')
             ->set('uid', 1)
-            ->sign(new Hmac\Sha256, $secret)
+            ->sign(new Hmac\Sha256, $this->secret)
             ->getToken();
 
-        $factory = new TokenFactory($expireTtl, $secret, $now);
+        $factory = $this->createFactory($now);
 
         $this->assertEquals($expectedToken, $factory->createForUser($user, ['foo' => 'bar']));
+    }
+
+    /** @test */
+    public function it_creates_token_from_request()
+    {
+        $user = (object)['id' => 1];
+        $expectedToken = $this->createFactory()->createForUser($user);
+
+        $request = $this->createMock(Request::class);
+        $request->expects($this->any())
+            ->method('header')
+            ->with($this->equalTo('authorization'))
+            ->willReturn("Bearer {$expectedToken}");
+
+        $token = $this->createFactory()->createFromRequest($request);
+
+        $this->assertEquals($expectedToken, $token);
+    }
+
+    /** @test */
+    public function it_creates_null_token_on_invalid_request()
+    {
+        $factory = $this->createFactory();
+
+        $request = $this->createMock(Request::class);
+        $request->expects($this->any())
+            ->method('header')
+            ->with($this->equalTo('authorization'))
+            ->willReturn(null);
+
+        $this->assertNull($factory->createFromRequest($request));
+    }
+
+    /** @test */
+    public function it_fails_on_invalid_token()
+    {
+        $factory = $this->createFactory();
+
+        $request = $this->createMock(Request::class);
+        $request->expects($this->any())
+            ->method('header')
+            ->with($this->equalTo('authorization'))
+            ->willReturn('Bearer soo');
+
+        $this->expectException(InvalidTokenException::class);
+        $factory->createFromRequest($request);
+    }
+
+    /** @test */
+    public function it_fails_when_token_is_not_verified()
+    {
+        $user = (object)['id' => 1];
+        $invalidToken = (string) $this->createFactory(null, 'notsosecret')->createForUser($user);
+
+        $request = $this->createMock(Request::class);
+        $request->expects($this->any())
+            ->method('header')
+            ->with($this->equalTo('authorization'))
+            ->willReturn("Bearer {$invalidToken}");
+
+        $this->expectException(InvalidTokenException::class);
+        $this->createFactory()->createFromRequest($request);
+    }
+
+    private function createFactory($now = null, string $secret = null): TokenFactory
+    {
+        return new TokenFactory($this->expireTtl, $secret ?? $this->secret, $now ?? new \DateTimeImmutable);
     }
 }
